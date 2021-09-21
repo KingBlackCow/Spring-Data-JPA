5d99dcd85d272cbdd281d228c8bde90efdcb4c86
KingBlackCow <sgs1159@naver.com>
2021-09-22 01:00:19 +0900

    도메인 모델 생성과 테스트

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,29 +1,48 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})//team은 출력하면 안됨 n+1문제//연관관계에서는 toString 쓰지말자
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "memeber_id")
     private Long id;
     private String username;
+    private int age;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)//지연로딩: 멤버만 조회할 때 딱 멤버만 조회하자 실제 값을 이용할 떄만 가져오자
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     //jpa는 빈생성자 무조건 있어야함 아무데서나 접근 막기위해 protected
     //entity는 기본적인 생성자가 있아야함
     //private로 설정하면 proxy에서 막혀버리는 경우가 생김
-    protected Member() {
+//    protected Member() {
+//    }
+
+    public Member(String username) {
+        this.username = username;
     }
 
-    public Member(String username){
+    public Member(String username, int age, Team team) {
         this.username = username;
+        this.age = age;
+        if(team!=null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);//반대쪽에 멤버도 반드시 바꾸기!!
     }
 }
